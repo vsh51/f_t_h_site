93f0be84a87ac7edcc972708da565e153e60697a
vsh51 <volshp125@gmail.com>
2019-08-12 09:15:06 +0300

    Edit secret key

--- hide/hide/settings.py
+++ hide/hide/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lf97&15n=$7$%m+48&&5*&-ray%=@%expsovqopvp3%z9%=-3)'
+SECRET_KEY = '****************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
